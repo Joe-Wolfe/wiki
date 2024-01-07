b64dbb801a2870894ea7f9a3c0d352641bcc7594
Joe Wolfe <JoeWolfe0527@gmail.com>
2024-01-06 22:02:21 -0500

    Fixed bug where pages where not being shown in the category pages

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
     os.environ.get('DATABASE_URL', 'postgresql:///wiki'))
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 app.app_context().push()
