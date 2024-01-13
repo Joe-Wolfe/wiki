b84d65fc0798445cab8ddcc526132472bda94713
Joe Wolfe <JoeWolfe0527@gmail.com>
2024-01-12 21:18:41 -0500

    Last minute changes to the navbar and forms.py to prevent the forms from being invalid.

--- forms.py
+++ forms.py
@@ -6,25 +6,29 @@ from wtforms.validators import DataRequired, Email, Length, EqualTo, Optional
 class UserAddForm(FlaskForm):
     """Form for adding users."""
 
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField('Username', validators=[
+                           DataRequired(), Length(max=20)])
     password = PasswordField('Password', validators=[Length(min=6)])
     confirm = PasswordField('Confirm Password', validators=[
                             DataRequired(), EqualTo('password', message='Passwords must match')])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    character_name = StringField('Character Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[
+                        DataRequired(), Email(), Length(max=50)])
+    character_name = StringField('Character Name', validators=[
+                                 DataRequired(), Length(max=30)])
 
 
 class UserLoginForm(FlaskForm):
     """Form for logging in users."""
 
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField('Username', validators=[
+                           DataRequired(), Length(max=20)])
     password = PasswordField('Password', validators=[Length(min=6)])
 
 
 class CategoryForm(FlaskForm):
     """Form for adding categories."""
 
-    name = StringField('Name', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired(), Length(max=30)])
     description = TextAreaField('Description', validators=[Optional()])
     created_by = HiddenField('Created By', validators=[DataRequired()])
 
@@ -32,15 +36,16 @@ class CategoryForm(FlaskForm):
 class PageForm(FlaskForm):
     """Form for adding pages."""
 
-    title = StringField('Title', validators=[DataRequired()])
+    title = StringField('Title', validators=[DataRequired(), Length(max=100)])
     category_id = SelectField('Category', validators=[DataRequired()])
-    synopsis = TextAreaField('Synopsis', validators=[Optional()])
+    synopsis = TextAreaField('Synopsis', validators=[
+                             Optional(), Length(max=280)])
     created_by = HiddenField('Created By', validators=[DataRequired()])
 
 
 class SectionForm(FlaskForm):
     """Form for adding sections."""
 
-    title = StringField('Title', validators=[DataRequired()])
+    title = StringField('Title', validators=[DataRequired(), Length(max=100)])
     position = HiddenField('Position', validators=[DataRequired()])
     body = TextAreaField('Content', validators=[Optional()])
