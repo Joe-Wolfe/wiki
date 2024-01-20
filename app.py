77a9fb935abdca09288f1f75c4c564bf48a7a8c3
Joe Wolfe <JoeWolfe0527@gmail.com>
2024-01-20 09:24:02 -0500

    added logging

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
     os.environ.get('DATABASE_URL', 'postgresql:///wiki'))
 
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_ECHO'] = True
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 
@@ -207,8 +207,6 @@ def deactivate_category(category_name):
         return redirect("/")
     category = Category.query.filter_by(name=category_name).first()
     category.is_active = False
-
-    """Unca"""
     db.session.commit()
 
     return redirect("/categories")
